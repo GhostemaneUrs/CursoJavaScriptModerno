6562280785e95ccc316b28c9a43399a005ac2ed8
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-09-14 11:09:47 -0500

    Aprendiendo funciones en JavaScript

---
+++ JavaScript/claseDos.js
@@ -0,0 +1,15 @@
+/* Tipo de funciones en JavaScript */
+
+/* Funciones declarativas */
+
+function suma(numeroUno, numeroDos) {
+    console.log(`La suma de los numeros ${numeroUno} y ${numeroDos} es: ` + (numeroUno + numeroDos));
+}
+suma(50, 60);
+
+/* Funciones expresivas */
+
+let resta = function (numeroUno, numeroDos) {
+    console.log(`La resta de los numeros ${numeroUno} y ${numeroDos} es: ` + (numeroUno - numeroDos));
+}
+resta(20, 10);
