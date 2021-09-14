7a107b9657ee52269b48cfe0e7d1a031e3b894da
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-09-14 09:22:25 -0500

    Aprendiendo valores primitivos, tipo de variables en JavaScript

--- JavaScriptBasico/claseUno.js
+++ JavaScriptBasico/claseUno.js
@@ -1,6 +1,7 @@
 /* Tipos de Valores en JavaScript */
 
 /* Valores primitivos */
+
 entero = 40;
 string = "Andres Jaramillo"
 validacionFalse = false
@@ -9,14 +10,27 @@ tipoVacio = null
 tipoVacioUndefined = undefined
 
 /* Saber que tipo de dato tengo */
-console.log(`Este valor ${entero} es de tipo: `  + typeof(entero))
-console.log(`Este valor ${string} es de tipo: `  + typeof(string))
-console.log(`Este valor ${validacionFalse} es de tipo: `  + typeof(validacionFalse))
-console.log(`Este valor ${validacionTrue} es de tipo: `  + typeof(validacionTrue))
-console.log(`Este valor ${tipoVacio} es de tipo: `  + typeof(tipoVacio))
-console.log(`Este valor ${tipoVacioUndefined} es de tipo: `  + typeof(tipoVacioUndefined))
+
+console.log(`Este valor ${entero} es de tipo: ` + typeof(entero))
+console.log(`Este valor ${string} es de tipo: ` + typeof(string))
+console.log(`Este valor ${validacionFalse} es de tipo: ` + typeof(validacionFalse))
+console.log(`Este valor ${validacionTrue} es de tipo: ` + typeof(validacionTrue))
+console.log(`Este valor ${tipoVacio} es de tipo: ` + typeof(tipoVacio))
+console.log(`Este valor ${tipoVacioUndefined} es de tipo: ` + typeof(tipoVacioUndefined))
 
 /* Tipo de variables en JavaScript */
-var nombre = "Andres"; //Variables inicializada
-var elementos = ["Computadora", "Celular"] //Tipo array
-var 
+
+var nombre = "Andres";
+
+/* Tipo array */
+
+var array = ["Computadora", "Celular"]
+
+/* Tipo Objeto */
+
+var persona = {
+    primerNombre: "Andres",
+    primerApellido: "Jaramillo",
+    edad: 22,
+    hobbies: "Aprender programacion"
+}
