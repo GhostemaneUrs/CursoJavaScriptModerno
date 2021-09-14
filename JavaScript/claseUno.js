74a3a64489d49a34f322c4db06581984e8ccd6f5
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-09-14 11:09:26 -0500

    Aprendiendo valores y variables en JavaScript

---
+++ JavaScript/claseUno.js
@@ -0,0 +1,36 @@
+/* Tipos de Valores en JavaScript */
+
+/* Valores primitivos */
+
+entero = 40;
+string = "Andres Jaramillo"
+validacionFalse = false
+validacionTrue = true
+tipoVacio = null
+tipoVacioUndefined = undefined
+
+/* Saber que tipo de dato tengo */
+
+console.log(`Este valor ${entero} es de tipo: ` + typeof(entero))
+console.log(`Este valor ${string} es de tipo: ` + typeof(string))
+console.log(`Este valor ${validacionFalse} es de tipo: ` + typeof(validacionFalse))
+console.log(`Este valor ${validacionTrue} es de tipo: ` + typeof(validacionTrue))
+console.log(`Este valor ${tipoVacio} es de tipo: ` + typeof(tipoVacio))
+console.log(`Este valor ${tipoVacioUndefined} es de tipo: ` + typeof(tipoVacioUndefined))
+
+/* Tipo de variables en JavaScript */
+
+let nombre = "Andres";
+
+/* Tipo array */
+
+let array = ["Computadora", "Celular"]
+
+/* Tipo Objeto */
+
+let persona = {
+    primerNombre: "Andres",
+    primerApellido: "Jaramillo",
+    edad: 22,
+    hobbies: "Aprender programacion"
+}
