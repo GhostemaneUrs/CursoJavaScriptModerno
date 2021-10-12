69a55439e6454de17db74a6e9259dde041cde711
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-10-12 18:49:07 -0500

    feat: prototipos en JavaScript

---
+++ JavaScript/prototipos.js
@@ -0,0 +1,8 @@
+/* Aprendiendo prototipos en JavaScript */
+
+/* Programacion orientada a objetos
+Clases => Modelo a seguir
+Objetos => Es una instancia de una clase
+Atributos => Es una caracteristica o propiedad del objeto (Son variables dentro de un objeto)
+Metodos => Son las acciones que un objeto puede realizar
+*/
