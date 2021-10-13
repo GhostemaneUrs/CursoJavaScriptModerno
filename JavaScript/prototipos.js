3412da3e2f9c96b144ce3e9cde43f1645722df7e
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-10-12 20:09:36 -0500

    feat: actualizando prototipos en JavaScript

--- JavaScript/prototipos.js
+++ JavaScript/prototipos.js
@@ -6,3 +6,26 @@ Objetos => Es una instancia de una clase
 Atributos => Es una caracteristica o propiedad del objeto (Son variables dentro de un objeto)
 Metodos => Son las acciones que un objeto puede realizar
 */
+
+/* Funcion constructora */
+
+function Persona(nombreCompleto, genero, edad) { /* ===> Esto es una instancia */
+    /* Atributos */
+    this.nombreCompleto = nombreCompleto;
+    this.genero = genero;
+    this.edad = edad;
+}
+
+/* Metodos agregados al prototipo */
+Persona.prototype.saludar = function () {
+    console.log(`${this.nombreCompleto} esta diciendo: Hola, ¿como estas?`)
+}
+
+Persona.prototype.comer = function () {
+    console.log(`La persona ${this.nombreCompleto} esta comiendo`)
+}
+
+const persona = new Persona("Andres Jaramillo ", "Masculino", 22);
+const personaDos = new Persona("Karina Miranda", "Masculino", 21);
+console.log(persona)
+console.log(personaDos)
