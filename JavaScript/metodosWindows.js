17bdf6b71ff67821e1366c9abdedf2676a67133a
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-10-25 12:09:29 -0500

    feat(root): metodos windows en JavaScript

---
+++ JavaScript/metodosWindows.js
@@ -0,0 +1,5 @@
+/* Metodos windows en JavaScript */
+
+let alerta = alert('Hola, esto es una alerta en JavaScript')
+let confirma = confirm('Hola, esto es una alerta confirmadora en JavaScript') /* Valida que sea falso o true */
+let aviso = prompt('Hola, esto es un aviso en JavaScript') /* Le permite al usuario ingresar un dato por medio de una alerta */
