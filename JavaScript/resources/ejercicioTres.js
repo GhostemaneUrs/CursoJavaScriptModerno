4ee4c1a512f4c7a8e1dc62f35a1cd0186977468b
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-10-26 13:11:23 -0500

    feat(root): ejercicioTres

---
+++ JavaScript/resources/ejercicioTres.js
@@ -0,0 +1,14 @@
+/* Programa una función que obtenga un numero aleatorio entre 501 y 600. */
+
+const numeroAleatorio = function (numeroMaximo, numeroMinimo) {
+    if (!numeroMaximo && !numeroMinimo) {
+        console.log('Los valores no pueden ser vacios')
+    } else if (typeof numeroMaximo !== 'number' & typeof numeroMinimo !== 'number') {
+        console.log('No se permite valores tipo string')
+    } else if (typeof numeroMaximo === 'number' & typeof numeroMinimo === 'number') {
+        let random = Math.round(Math.random() * (numeroMaximo - numeroMinimo) + numeroMinimo)
+        console.log(`El numero random es: ${random}`)
+    }
+}
+
+numeroAleatorio(501, 601)
