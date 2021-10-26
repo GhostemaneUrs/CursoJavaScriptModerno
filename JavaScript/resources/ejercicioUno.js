bbe5874d105a0e09a4d3876680a62ef806adfee8
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-10-26 10:59:46 -0500

    feat(root): update ejercicioUno

--- JavaScript/resources/ejercicioUno.js
+++ JavaScript/resources/ejercicioUno.js
@@ -1,22 +1,24 @@
 /* Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10. */
 
 const contarCaracteres = function (cadena) {
-    if (cadena === null) {
-        console.log('Escriba una cadena, por favor')
+    if (!cadena) {
+        console.log('La cadena no puede ir vacia')
     } else if (typeof cadena !== 'string') {
         console.log('El valor que ingreso no es de tipo cadena, por favor escriba uno')
-    } else if (typeof cadena === 'string') {
+    } else if (typeof cadena === 'string' && cadena.length > 0) {
         console.log(`La cadena "${cadena}", tiene una cantidad de caracteres: ${cadena.length}`)
     }
 }
 
-contarCaracteres('Hola')
+contarCaracteres()
 
 /* Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola" */
 
 const recortarCaracteres = function (cadena, cantidad) {
     if (isNaN(cantidad)) {
-        console.log('Por favor la cantidad no puede ir vacia')
+        console.log('La cantidad no puede ir vacia')
+    } else if (!cadena) {
+        console.log('La cadena no puede ir vacia')
     } else if (typeof cantidad !== 'number') {
         console.log('Por favor escriba un numero valido')
     } else if (typeof cadena !== 'string') {
@@ -33,7 +35,7 @@ recortarCaracteres('Hola mundo', 4)
 /* Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal']. */
 
 const almacenarCaracteresArray = function (cadena) {
-    if (cadena === null) {
+    if (!cadena) {
         console.log('La cadena no puede ir vacia')
     } else if (typeof cadena !== 'string') {
         console.log('Solo se permiten valores tipo string')
@@ -53,6 +55,8 @@ const repetirCaracteres = function (cadena, cantidad) {
     let repetirCadena = ''
     if (isNaN(cantidad)) {
         console.log('La cantidad no puede ir vacia')
+    } else if (!cadena) {
+        console.log('La cadena no puede ir vacia')
     } else if (typeof cadena !== 'string') {
         console.log('Solo se permiten valores tipo string')
     } else if (typeof cadena === 'string') {
