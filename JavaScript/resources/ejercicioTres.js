e228d7fa33524a148badaecbc606d753a9a07bd0
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-10-27 12:02:15 -0500

    feat(root): update ejercicioTres

--- JavaScript/resources/ejercicioTres.js
+++ JavaScript/resources/ejercicioTres.js
@@ -13,4 +13,4 @@ const numeroAleatorio = function (numeroMaximo, numeroMinimo) {
 
 numeroAleatorio(501, 601)
 
-/* Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true. */
+
