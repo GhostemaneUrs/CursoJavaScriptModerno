7f08124d894bd69bc15794a04c3691107eb51e49
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-10-30 13:20:40 -0500

    feat(root): update ejercicioUno

--- JavaScript/resources/ejercicioUno.js
+++ JavaScript/resources/ejercicioUno.js
@@ -1,6 +1,6 @@
 /* Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10. */
 
-const contarCaracteres = function (cadena) {
+const contarCaracteres = (cadena) => {
     if (!cadena) {
         console.log('La cadena no puede ir vacia')
     } else if (typeof cadena !== 'string') {
@@ -14,7 +14,7 @@ contarCaracteres()
 
 /* Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola" */
 
-const recortarCaracteres = function (cadena, cantidad) {
+const recortarCaracteres = (cadena, cantidad) => {
     if (isNaN(cantidad)) {
         console.log('La cantidad no puede ir vacia')
     } else if (!cadena) {
@@ -34,7 +34,7 @@ recortarCaracteres('Hola mundo', 4)
 
 /* Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal']. */
 
-const almacenarCaracteresArray = function (cadena) {
+const almacenarCaracteresArray = (cadena) => {
     if (!cadena) {
         console.log('La cadena no puede ir vacia')
     } else if (typeof cadena !== 'string') {
@@ -50,8 +50,7 @@ console.log(almacenarCaracteresArray('Hola'))
 
 /* Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo. */
 
-
-const repetirCaracteres = function (cadena, cantidad) {
+const repetirCaracteres = (cadena, cantidad) => {
     let repetirCadena = ''
     if (isNaN(cantidad)) {
         console.log('La cantidad no puede ir vacia')
