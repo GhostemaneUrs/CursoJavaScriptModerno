c075bc303398387519da124b51a1bf097fed3e57
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-11-13 13:19:48 -0500

    feat(root): update loops

--- JavaScript/loops.js
+++ JavaScript/loops.js
@@ -26,6 +26,14 @@ for (let i = 0; i < numeros.length; i++) {
     console.log("Numeros: " + numeros[i])
 }
 
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(`Los numeros pares son: ${i}`)
+    } else {
+        console.log(`Los numeros impares son: ${i}`)
+    }
+}
+
 /* For in especial para objeto */
 const persona = {
     primerNombre: "Andres",
