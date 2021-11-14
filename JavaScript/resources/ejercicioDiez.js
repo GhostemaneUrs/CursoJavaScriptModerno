63e7e3c033055894f23df66b1687a531b5273ad8
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-11-14 11:28:40 -0500

    feat(root): ejercicioDiez

---
+++ JavaScript/resources/ejercicioDiez.js
@@ -0,0 +1,14 @@
+/* Ejercicio Fizz Buzz */
+/* 3 6 9 12 = Fizz */
+/* 5 10 15 20 = Buzz */
+/* 15 30 45 = Fizz Buzz */
+
+for (let index = 1; index <= 100; index++) {
+    if (index % 3 === 0 && index % 5 === 0) {
+        console.log(`El numero ${index} = Fizz Buzz!!`);
+    } else if (index % 3 === 0) {
+        console.log(`El numero ${index} = Fizz`);
+    } else if (index % 5 === 0) {
+        console.log(`El numero ${index} = Buzz`);
+    }
+}
