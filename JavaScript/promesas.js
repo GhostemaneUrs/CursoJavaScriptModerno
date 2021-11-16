29f90fe26bd32401d1ba1f2f6ef1d291e211b823
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-11-16 12:25:25 -0500

    feat(root): promesas en JavaScript

---
+++ JavaScript/promesas.js
@@ -0,0 +1,33 @@
+/* Promesas en JavaScript */
+
+const elevarAlCuadrado = (numero) => {
+    return new Promise((resolve, reject) => {
+        if (!numero) {
+            reject(`Error: ${numero} es un valor vacio, escriba un numero, por favor`);
+        } else if (typeof numero !== 'number') {
+            reject(`Error: el valor ingresado "${numero}" no es un numero`);
+        } else if (typeof numero === 'number') {
+            setTimeout(() => {
+                resolve({
+                    numero, resultado: numero * numero
+                });
+            }, 0 | Math.random() * 100)
+        }
+    });
+}
+
+elevarAlCuadrado(1).then((obj) => {
+    console.log(`Numero: ${obj.numero}, su resultado elevado es: ${obj.resultado}`);
+    return elevarAlCuadrado(2);
+}).then((obj) => {
+    console.log(`Numero: ${obj.numero}, su resultado elevado es: ${obj.resultado}`);
+    return elevarAlCuadrado(3);
+}).then((obj) => {
+    console.log(`Numero: ${obj.numero}, su resultado elevado es: ${obj.resultado}`);
+    return elevarAlCuadrado(4);
+}).then((obj) => {
+    console.log(`Numero: ${obj.numero}, su resultado elevado es: ${obj.resultado}`);
+    return elevarAlCuadrado();
+}).then((obj) => {
+    console.log(`Numero: ${obj.numero}, su resultado elevado es: ${obj.resultado}`);
+}).catch(error => console.error(error));
